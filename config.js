bc5e2b1733e512b356db530119c756b9983a9584
FireNSpark <unfavorablejosh@gmail.com>
2025-08-02 07:29:46 -0400

    Update config.js

--- config.js
+++ config.js
@@ -1,1 +1,13 @@
+// config.js
+let CONFIG = {};
+
+async function loadConfig() {
+  const res = await fetch('/env.json');
+  const data = await res.json();
+  CONFIG = {
+    OPENAI_KEY: data.OPENAI_KEY,
+    GITHUB_TOKEN: data.GITHUB_TOKEN,
+    GIST_ID: data.GIST_ID
+  };
+}
 

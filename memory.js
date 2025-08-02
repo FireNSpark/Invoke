04c79f30eb0dd2a54f419869c91436db737e0bad
FireNSpark <unfavorablejosh@gmail.com>
2025-08-02 07:31:40 -0400

    Update memory.js

--- memory.js
+++ memory.js
@@ -1,1 +1,27 @@
+// memory.js
+async function loadMemory() {
+  const res = await fetch(`https://api.github.com/gists/${CONFIG.GIST_ID}`, {
+    headers: { Authorization: `Bearer ${CONFIG.GITHUB_TOKEN}` }
+  });
+  const data = await res.json();
+  return JSON.parse(data.files["memory.json"].content);
+}
+
+async function saveMemory(memoryObj) {
+  await fetch(`https://api.github.com/gists/${CONFIG.GIST_ID}`, {
+    method: "PATCH",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${CONFIG.GITHUB_TOKEN}`
+    },
+    body: JSON.stringify({
+      files: {
+        "memory.json": {
+          content: JSON.stringify(memoryObj, null, 2)
+        }
+      }
+    })
+  });
+}
+
 

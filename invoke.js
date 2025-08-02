4ecde7aa5a25eca3dd0fb97be6c976542e032993
FireNSpark <unfavorablejosh@gmail.com>
2025-08-02 07:33:14 -0400

    Update invoke.js

--- invoke.js
+++ invoke.js
@@ -1,1 +1,18 @@
 
+// invoke.js
+async function handleInput() {
+  if (!CONFIG.OPENAI_KEY) await loadConfig();
+
+  const input = document.getElementById("userInput").value;
+  const output = document.getElementById("output");
+
+  output.innerHTML += `<div><b>You:</b> ${input}</div>`;
+
+  const response = await fetchGPT(input);
+
+  output.innerHTML += `<div><b>INVOKE:</b> ${response}</div>`;
+  speak(response);
+
+  document.getElementById("userInput").value = "";
+}
+
